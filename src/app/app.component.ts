6a4d42c181430ecc8410f83eae3f64709925b39d
abedjamous <jamous.hameed94@gmail.com>
2021-08-11 22:33:53 -0500

    setup the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { User } from './user';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'td-form';
+  topics= ['Angular', 'React', 'Vue'];
+
+  onClick(f: NgForm) {
+  console.log(f)
+  }
+
+  userModel = new User('Abed', 'Abed@test.com',7325555555, '','morning', true);
 }
